d2f094cb210eb7cddbf37530a8f187c79c63fea4
PatriciaPoveda <patriciapovedaboto@gmail.com>
2021-03-15 01:27:42 +0100

    add menu

--- src/js/btnToggle.js
+++ src/js/btnToggle.js
@@ -1,15 +1,40 @@
 const btnMenu = document.querySelector(".js-btnMenu");
 const navContainer = document.querySelector(".js-nav");
 const navLink = document.querySelector(".js-link");
+const navBar = document.querySelector(".js-bar__nav");
 
 const openNav = () => {
-  navContainer.classList.toggle("hidden");
+  if (navContainer.classList.contains("hidden")) {
+    navContainer.classList.remove("hidden");
+    navContainer.classList.add("open");
+    navContainer.classList.remove("close");
+  } else {
+    navContainer.classList.add("close");
+    navContainer.classList.remove("open");
+    navContainer.classList.add("hidden");
+  }
 };
 
 btnMenu.addEventListener("click", openNav);
 
 const closeNav = () => {
-  navContainer.classList.toggle("hidden");
+  navContainer.classList.add("close");
+  navContainer.classList.remove("open");
+  navContainer.classList.add("hidden");
 };
 
-navLink.addEventListener("click", closeNav);
+navContainer.addEventListener("mouseleave", closeNav);
+
+const closeNavLink = () => {
+  if (navContainer.classList.contains("hidden")) {
+    navContainer.classList.remove("hidden");
+    navContainer.classList.add("open");
+    navContainer.classList.remove("close");
+  } else {
+    navContainer.classList.add("close");
+    navContainer.classList.remove("open");
+    navContainer.classList.add("hidden");
+  }
+};
+
+navLink.addEventListener("click", closeNavLink);
