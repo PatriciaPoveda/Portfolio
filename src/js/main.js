9fe8fd26bd6c63724e1faf76ceead3c0d3b1afef
PatriciaPoveda <patriciapovedaboto@gmail.com>
2021-04-07 22:27:27 +0200

    fixed animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
 "use strict";
-const btnInit = document.querySelector(".js-menu-wrapper__copy--home");
-
-const paintBtn = () => {
-  btnFa.classList.add("scrollUp");
-  toggle.classList.add("scrollUp");
-};
-
-btnInit.addEventListener("click", paintBtn);
